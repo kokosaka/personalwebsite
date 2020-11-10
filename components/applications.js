200bf0d080430ceea57cf623c4dfaa998abc6ecf
Koboh Kosaka <kobohkosaka@outlook.com>
2020-11-10 01:15:50 -0800

    Add to applications page

--- components/applications.js
+++ components/applications.js
@@ -14,6 +14,7 @@ const Applications = (props) => {
         <div className="project-desc">
           <h2>Nutrific</h2>
           <p>In Progress</p>
+          <div className="project-tools">Google FireBase - Redux - React Native - Expo</div>
         </div>
         <div className="project-links">
           {/* {props.fitnessTailorGif ?
@@ -33,8 +34,9 @@ const Applications = (props) => {
         </a>}
         <div className="project-desc">
           <h2>Know It All Application</h2>
-          <p>With a team of four, we built a trivia game application that could be played on iOS and Android. Built in React Native, I was in charge of implementing the category selection screen. The application sends a get request to a trivia API and receives/parses the object in the frontend.
+          <p>A trivia game application that could be played on iOS or Android, meant to be played locally. Built in React Native, players can choose their team name, color, and the category they want to test their knowledge on.
           </p>
+          <div className="project-tools">React Native - Expo</div>
         </div>
         <div className="project-links">
           {props.knowItAllGif ?
@@ -51,6 +53,7 @@ const Applications = (props) => {
         <div className="project-desc">
           <h2>Alo Yoga Mock (System Design)</h2>
           <p>I inherited a team members application (a carousel of products) with a focus on improving the backend. With an initial throughput of 8 requests per second (rps), I set a goal to have the service handle 1000 rps. The backend was built with MongoDB and I wanted to compare the same service but with a SQL database (PostgreSQL). After seeding and indexing the databases with 10 million products, I monitored the services with New Relic, stress testing with k6. The PostgreSQL database was handling more at 244 rps so I decided to move forward with the SQL database. I deployed the database and the service to an AWS EC2 instance; stress tested with loader.io. The service handled up to 594 rps with a stress test configured at 1000 clients sending a request every second for 1 minute. I decided to horizontally scale the service and deployed another instance of the service and a load balancer proxy (nginx). Once deployed and tested, the goal of 1000 rps was achieved; with a 0% error rate and 68ms latency.</p>
+          <div className="project-tools">JavaScript - PostgreSQL - AWS</div>
         </div>
         <div className="project-links">
           <a href="https://github.com/Dumplings-Reloaded/related-items">
@@ -68,6 +71,7 @@ const Applications = (props) => {
         <div className="project-desc">
           <h2>Glossier Mock (Front End Focused)</h2>
           <p>Teaming up with three other engineers, we recreated an item detail page mocking that of Glossier. Aside from creating a simple Express.js server and a MySQL database seeded from my AWS S3 bucket that held mock products; this application was focused heavily on the front-end. I chose to recreate the navigation bar because I wanted to challenge myself in solving the search function within the navigation bar. Glossier's search tab would filter the products upon the user's key press. This was achieved by sending the state of the search form to the back-end; querying the database at each keystroke. As for the best seller tabs, I designed the application to send a get request at the onHover of each tab to the corresponding category of makeup.</p>
+          <div className="project-tools">JavaScript - MySQL - Express - AWS - HTML - CSS</div>
         </div>
         <div className="project-links">
           {props.navBarGif ?
--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,37 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import Navbar from './navbar';
 // import Footer from './footer';
 
-const Layout = (props) => (
-  <div className="body">
-    <Head>
-      <title>Koboh Kosaka</title>
-    </Head>
-    <Navbar color={props.color} scrollToRef={props.scrollToRef}/>
-    {props.children}
-  </div>
+const Layout = (props) => {
+  const [prevScroll, setPrevScroll] = useState(0);
+  const [showNav, setShowNav] = useState(true);
+
+  const handleScroll = () => {
+    const currentScroll = window.scrollY;
+    if (prevScroll > currentScroll) {
+      setShowNav(true);
+    } else {
+      setShowNav(false);
+    }
+    setPrevScroll(currentScroll);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    //using addEventListeners in useEffect needs a return statement otherwise listener is removed immediately
+    return () => window.removeEventListener("scroll", handleScroll);
+});
+
+  return (
+    <div className="body" >
+      <Head>
+        <title>Koboh Kosaka</title>
+      </Head>
+        <Navbar id={showNav ? 'slideIn' : 'slideOut'} color={props.color} scrollToRef={props.scrollToRef} />
+      {props.children}
+    </div>
 );
+}
 
 export default Layout;
