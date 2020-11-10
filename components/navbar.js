2b860547e09d098a995a806ac144322d0cf7d9f4
Koboh Kosaka <kobohkosaka@outlook.com>
2020-11-10 01:14:39 -0800

    Implement navbar slideIn upon scroll up feature

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Social from './social';
 
 const Navbar = (props) => (
-  <div className="navbar">
+  <div className="navbar" id={props.id}>
     {/* <Link href="/"><a className="navbar-item">home</a></Link> */}
     <ul className="navbar-list">
       <li ><a className="navbar-item" id="firstRef" onClick={props.scrollToRef}>about</a></li>
