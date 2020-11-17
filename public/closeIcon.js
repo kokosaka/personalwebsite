d57e5e22d61e0ab03dc2a6e705f485a5493e24c3
Koboh Kosaka <kobohkosaka@outlook.com>
2020-11-16 20:31:52 -0800

    Refactor color of mobile icons

--- public/closeIcon.js
+++ public/closeIcon.js
@@ -1,7 +1,7 @@
 const closeIcon = (props) => (
   <svg className="closeIcon" width="209" height="210" viewBox="0 0 209 210" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <line x1="25.7484" y1="25.1345" x2="184.135" y2="185.252" stroke="black" stroke-width="35" stroke-linecap="round"/>
-    <line x1="184.003" y1="24.7487" x2="24.7487" y2="184.003" stroke="black" stroke-width="35" stroke-linecap="round"/>
+    <line className="mobileIconLine" x1="25.7484" y1="25.1345" x2="184.135" y2="185.252" stroke="black" stroke-width="35" stroke-linecap="round"/>
+    <line className="mobileIconLine"x1="184.003" y1="24.7487" x2="24.7487" y2="184.003" stroke="black" stroke-width="35" stroke-linecap="round"/>
   </svg>
 )
 
