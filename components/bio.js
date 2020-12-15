2b6ac9aa452cf22431911c3b87bbf1bd31406742
Koboh Kosaka <kobohkosaka@outlook.com>
2020-12-15 15:47:41 -0800

    Add to bio

--- components/bio.js
+++ components/bio.js
@@ -10,10 +10,22 @@ const Bio = (props) => (
         and I am a software engineer<br></br>
         located in Los Angeles, California.</p>
       <p>
-        Whether it's building websites, phone applications, or intricate algorithms, I enjoy collaborating with other engineers and solving difficult problems. Considering the resources I have at hand, I enjoy taking on challenges from different angles to improve on efficiency. I am intrigued by new technologies and wonder about the next steps I can take in building a better self and a better future.
+        Working on several applications in an agile team environment, I have acquired solid web development, critical thinking, and effective communication experience. While keeping "learn new technologies" high on my priorities, I love to dig deep into what I can do with well established stacks, inching closer to mastery. I strive to code with efficiency, keeping in mind reusability and scalability.
       </p>
       <p>
-        As of August 2020, I am currently looking for a position to contribute to an organization and create new software.
+        Working with other engineers, I have implemented several features for modular and scalable applications. I have a strong experience with the MERN stack, building several Restful APIs with the backend built in MongoDB, Express and Node.js and the frontend built in JavaScript and React. However, depending on the application and it's use case, I have implemented other tech stacks such as PostgreSQL, MySQL, supplemented React with Redux, applying cloud-based storage with AWS EC2, even implementing serverless architecture with Google Firebase.
+      </p>
+      <p>
+        ‣ August, 2020 - Graduated from SEI bootcamp and now looking for a position to contribute to an organization and create new software.
+      </p>
+      <p>
+        ‣ May, 2020 - Started software engineering bootcamp.
+      </p>
+      <p>
+        ‣ February, 2020 - Departed my position as a Food Safety Manager and began devoting all of my time learning computer science fundamentals through online resources.
+      </p>
+      <p>
+        ‣ June, 2018 - Graduated from University of California, Irvine with a BA in Economics.
       </p>
     </div>
     <button className='scroll-button' id="secondRef" onClick={props.scrollToRef}>cd</button>
