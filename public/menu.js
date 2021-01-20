d0ee93fcd5b35c627997874557dfd438de280532
Koboh Kosaka <kobohkosaka@outlook.com>
2021-01-20 12:05:45 -0800

    Refactor svg properties to clear warnings

--- public/menu.js
+++ public/menu.js
@@ -1,8 +1,8 @@
 const Menu = (props) => (
 <svg className="menuIcon" width="201" height="196" viewBox="0 0 201 196" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <line className="mobileIconLine" x1="17.5" y1="17.5" x2="180.5" y2="17.5" stroke="black" stroke-width="35" stroke-linecap="round"/>
-  <line className="mobileIconLine" x1="17.5" y1="97.5" x2="182.5" y2="97.5" stroke="black" stroke-width="35" stroke-linecap="round"/>
-  <line className="mobileIconLine" x1="19.5088" y1="177.509" x2="182.51" y2="177.591" stroke="black" stroke-width="35" stroke-linecap="round"/>
+  <line className="mobileIconLine" x1="17.5" y1="17.5" x2="180.5" y2="17.5" stroke="black" strokeWidth="35" strokeLinecap="round"/>
+  <line className="mobileIconLine" x1="17.5" y1="97.5" x2="182.5" y2="97.5" stroke="black" strokeWidth="35" strokeLinecap="round"/>
+  <line className="mobileIconLine" x1="19.5088" y1="177.509" x2="182.51" y2="177.591" stroke="black" strokeWidth="35" strokeLinecap="round"/>
 </svg>
 )
 
