717a5e4e4e33678ca8425b7c935bc6f04793905c
Koboh Kosaka <kobohkosaka@outlook.com>
2021-05-27 14:44:32 -0700

    Modify CSS/Debug mobile navbar to desktop switching

--- components/layout.js
+++ components/layout.js
@@ -2,12 +2,13 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import MobileNav from "./mobileNav";
 import Navbar from "./navbar";
+import MenuIcon from "../public/menu";
+
 // import Footer from './footer';
 
 const Layout = (props) => {
   const [prevScroll, setPrevScroll] = useState(0);
   const [showNav, setShowNav] = useState(true);
-  const [isMobile, setIsMobile] = useState(false);
 
   const handleScroll = () => {
     const currentScroll = window.scrollY;
@@ -20,10 +21,8 @@ const Layout = (props) => {
   };
 
   useEffect(() => {
-    let isMobile = window.matchMedia("screen and (max-width: 975px)").matches;
-    if (isMobile) {
-      setIsMobile(true);
-    }
+    //   setIsMobile(true);
+    // }
     window.addEventListener("scroll", handleScroll);
     //using addEventListeners in useEffect needs a return statement otherwise listener is removed immediately
     return () => window.removeEventListener("scroll", handleScroll);
@@ -34,17 +33,14 @@ const Layout = (props) => {
       <Head>
         <title>Koboh Kosaka</title>
       </Head>
-      {isMobile ? (
-        <MobileNav
-          id={showNav ? "slideIn" : "slideOut"}
-          scrollToRef={props.scrollToRef}
-        />
-      ) : (
-        <Navbar
-          id={showNav ? "slideIn" : "slideOut"}
-          scrollToRef={props.scrollToRef}
-        />
-      )}
+      <MobileNav
+        id={showNav ? "slideIn" : "slideOut"}
+        scrollToRef={props.scrollToRef}
+      />
+      <Navbar
+        id={showNav ? "slideIn" : "slideOut"}
+        scrollToRef={props.scrollToRef}
+      />
       {props.children}
     </div>
   );
